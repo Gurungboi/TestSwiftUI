0f137dfba4a142a2042652e341ad6f948b490849
Sunil Gurung <sunil@prezzee.com.au>
2021-05-27 00:08:34 +1000

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Sunil on 27/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
